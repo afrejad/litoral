5c5bf91677085e1a7af76d63e545d8ae4a6bac89
Adrian Freja D <afrejad@gmail.com>
2026-03-14 13:29:11 -0500

    Configurar despliegue en GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  site: "https://afrejad.github.io",
+  base: "/litoral",
   vite: {
     plugins: [tailwindcss()],
   },
